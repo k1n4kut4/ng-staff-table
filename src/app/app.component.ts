93c83e3deb452662d3c17f0bdbd898bd3b0436ef
Lucius Mathematica <52294958+k1n4kut4@users.noreply.github.com>
2019-07-23 16:11:31 +0100

    table data interface added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,14 @@ import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatTable } from '@angular/material';
 
+export interface dataElement {
+  Name: string;
+  Company: string;
+  Gender: string;
+}  
+
+const ELEMENT_DATA: dataElement[] = [];
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
